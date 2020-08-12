0fb6b3a070724083b1b8abb53948e2b8cfeae095
KerronKing <king_of_palmiste@hotmail.com>
2020-08-12 17:38:17 -0400

    chore: add initial dictionary searcher script logic

---
+++ Interactive Dictionary/app.py
@@ -0,0 +1,15 @@
+import json
+
+data = json.load(open('data.json'))
+
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return 'This word cannot be found'
+
+
+word = input("Enter a word: ")
+print(translate(word))
