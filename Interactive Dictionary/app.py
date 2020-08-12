066bd860c18c041517725eb1db263378684a85bb
KerronKing <king_of_palmiste@hotmail.com>
2020-08-12 18:28:54 -0400

    chore: complete word search scipt

--- Interactive Dictionary/app.py
+++ Interactive Dictionary/app.py
@@ -1,15 +1,39 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
 
 def translate(word):
     word = word.lower()
+    proper = word.capitalize()
+    upper = word.upper()
+
     if word in data:
         return data[word]
+    elif proper in data:
+        return data[proper]
+    elif upper in data:
+        return data[upper]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input('Did you mean %s? Enter "y" for yes and "n" for no: '
+                   % get_close_matches(word, data.keys())[0])
+        if yn == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == 'n':
+            return 'Re-run the script to find the word you\'re looking for'
+        else:
+            return 'Invalid query entered'
     else:
         return 'This word cannot be found'
 
 
 word = input("Enter a word: ")
-print(translate(word))
+
+output = translate(word)
+
+if type(output) == list:
+    for entry in output:
+        print(entry)
+else:
+    print(output)
